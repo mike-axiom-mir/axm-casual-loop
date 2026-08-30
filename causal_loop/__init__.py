9762115c39816d5f18a5d76bf1ceaed3d4a7bf8c
mike-axiom-mir <miketobi90@gmail.com>
2026-08-30 20:40:34 +0200

    Expose causal loop v0.01 API

---
+++ causal_loop/__init__.py
@@ -0,0 +1,14 @@
+from .engine import CausalLoopEngine, Invariant, LoopSpec, Module, commit_receipt_to_history, deterministic_hash
+from .train_platform import LOOP_ID, build_engine, initial_state
+
+__all__ = [
+    "CausalLoopEngine",
+    "Invariant",
+    "LoopSpec",
+    "Module",
+    "commit_receipt_to_history",
+    "deterministic_hash",
+    "LOOP_ID",
+    "build_engine",
+    "initial_state",
+]
