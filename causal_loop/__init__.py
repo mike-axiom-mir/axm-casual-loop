8fc46b8234fb7ac10ad25d015d8393595fb856bd
mike-axiom-mir <miketobi90@gmail.com>
2026-08-30 21:52:09 +0200

    Export non-authoritative observer projection

--- causal_loop/__init__.py
+++ causal_loop/__init__.py
@@ -1,4 +1,5 @@
 from .engine import CausalLoopEngine, Invariant, LoopSpec, Module, TimedInfluence, commit_receipt_to_history, deterministic_hash
+from .observer import OBSERVER_SCHEMA, project_receipt
 from .train_platform import LOOP_ID, build_engine, initial_state
 
 __all__ = [
@@ -9,6 +10,8 @@ __all__ = [
     "TimedInfluence",
     "commit_receipt_to_history",
     "deterministic_hash",
+    "OBSERVER_SCHEMA",
+    "project_receipt",
     "LOOP_ID",
     "build_engine",
     "initial_state",
